213340cdbfc7150059ccb7d08907f7f94e5cb1b8
Juan Blanco Moyano <juanblancomoyano@gmail.com>
2026-05-12 15:25:41 +0100

    add flask-cors configuration

--- app/__init__.py
+++ app/__init__.py
@@ -1,22 +1,33 @@
 from flask import Flask
+from flask_cors import CORS
 from .config import Config
 from .extensions import db, jwt, migrate
 
 
 def create_app():
     app = Flask(__name__)
+
+    CORS(
+        app,
+        resources={r"/*": {"origins": "http://localhost:5173"}}
+    )
+
+    #config
     app.config.from_object(Config)
 
+    #extensions
     db.init_app(app)
     jwt.init_app(app)
     migrate.init_app(app, db)
 
+    #register blueprints
     from app import models
     from app.routes.absences import absences_bp
     from app.routes.auth import auth_bp
     from app.routes.distributions import distributions_bp
     from app.routes.employees import employees_bp
 
+    
     app.register_blueprint(absences_bp)
     app.register_blueprint(auth_bp)
     app.register_blueprint(distributions_bp)
