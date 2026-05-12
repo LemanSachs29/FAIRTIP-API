83cb002c04812444453ef8f24f6b169740f520ea
Juan Blanco Moyano <juanblancomoyano@gmail.com>
2026-05-12 17:41:47 +0100

    resources for Cors updated

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,11 @@ def create_app():
 
     CORS(
         app,
-        resources={r"/*": {"origins": "http://localhost:5173"}}
+        resources={r"/*": {"origins": [
+            "http://localhost:5173",
+            "http://127.0.0.1:5173"
+        ]}},
+        supports_credentials=True
     )
 
     #config
