4d76fc9db11d0c15f8a1ff57961288dfa9b841b4
Juan Blanco Moyano <juanblancomoyano@gmail.com>
2026-05-13 16:45:12 +0100

    Deployment config

--- app/config.py
+++ app/config.py
@@ -8,3 +8,13 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv("SECRET_KEY")
     JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", SECRET_KEY)
+
+    FRONTEND_URL = os.getenv("FRONTEND_URL")
+
+    CORS_ORIGINS = [
+        "http://localhost:5173",
+        "http://127.0.0.1:5173",
+    ]
+
+    if FRONTEND_URL:
+        CORS_ORIGINS.append(FRONTEND_URL)
